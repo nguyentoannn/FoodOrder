584407274a0b48d4c15bfe821dc6f556bdd874fe
orgin <https://github.com/nguyentoannn/OrderFoods>
2022-10-15 16:59:02 +0700

    new item

--- app/src/main/java/com/example/foodorder/MainActivity.java
+++ app/src/main/java/com/example/foodorder/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.foodorder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //variable
+    TextView btn_getStarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_getStarted = findViewById(R.id.txt_title);
+        btn_getStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, HomeActivity.class));
+            }
+        });
+
     }
 }
