1bbad9bb27c4815fe8e3e51be053090df2cf7414
orgin <https://github.com/nguyentoannn/OrderFoods>
2022-10-15 19:26:58 +0700

    update item

--- app/src/main/java/com/example/foodorder/AdminCreateProductActivity.java
+++ app/src/main/java/com/example/foodorder/AdminCreateProductActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class RegisterActivity extends AppCompatActivity {
+public class AdminCreateProductActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_admin_create_product);
     }
 }
--- app/src/main/java/com/example/foodorder/AdminEditProductActivity.java
+++ app/src/main/java/com/example/foodorder/AdminEditProductActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CreateProductActivity extends AppCompatActivity {
+public class AdminEditProductActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_product);
+        setContentView(R.layout.activity_admin_edit_product);
     }
 }
